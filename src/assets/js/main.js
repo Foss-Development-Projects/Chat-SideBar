071a33e802e240f79fa881eb4a523c01bd81f46e
Foster <72973015+Foster0123@users.noreply.github.com>
2023-02-10 01:44:01 +0530

    Fluid Sidebar Commit 1

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+const sideBar = document.getElementById("sidebar")
+const sideBarMenu = document.getElementById("sidebar-menu")
+
+sideBarMenu.addEventListener("click", () => {
+	sideBar.style.width = "0";
+})
