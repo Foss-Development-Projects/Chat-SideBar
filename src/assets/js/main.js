5b203623919fa77f6fb96e7ffd693ec6a5da399e
Foster <72973015+Foster0123@users.noreply.github.com>
2023-02-10 09:50:46 +0530

    Fluid Sidebar Commit 2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,30 @@
-const sideBar = document.getElementById("sidebar")
-const sideBarMenu = document.getElementById("sidebar-menu")
+const sideBar = document.getElementById('sidebar');
+const sideBarMenu = document.getElementById('sidebar-menu');
 
-sideBarMenu.addEventListener("click", () => {
-	sideBar.style.width = "0";
-})
+const chatItemMenuToggle = document.getElementsByClassName("chat-item-menu-toggle");
+const chatItemMenu = document.querySelectorAll("aside[data-chat-menu]")
+console.log(chatItemMenu)
+const openChatMenu = () => {
+    for (let i = 0; i < chatItemMenuToggle.length; i++) {
+        chatItemMenuToggle[i].addEventListener("click", () => {
+            if (window.getComputedStyle(chatItemMenu[i]).getPropertyValue("transform").match(/(-?[0-9\.]+)/g)[0] == 0) {
+                chatItemMenu[i].style.transform = "scale(1)"
+            }
+            else {
+                chatItemMenu[i].style.transform = "scale(0)"
+            }
+            console.log()
+        })
+        document.addEventListener('click', (event) => {
+            const withinBoundaries = event.composedPath().includes(chatItemMenuToggle[i])
+
+            if (!withinBoundaries) {
+                chatItemMenu[i].style.transform = "scale(0)"
+            }
+        })
+    }
+}
+openChatMenu()
+sideBarMenu.addEventListener('click', () => {
+    sideBar.style.width = '0';
+});
